a6e4defd4368b96c4b15e281e7ea31249b1f688c
Thijs Lucassen <thijs@nessacul.nl>
2016-10-19 14:24:08 +0200

    Intial commit

---
+++ Bridges/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bridges
+//
+//  Created by Thijs Lucassen on 19-10-16.
+//  Copyright © 2016 TL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
