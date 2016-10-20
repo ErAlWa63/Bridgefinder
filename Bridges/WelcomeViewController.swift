1067f5bede655277025c25ccb72c0819dfe433d7
Thijs Lucassen <thijs@nessacul.nl>
2016-10-20 14:28:56 +0200

    Added outlets and concepttext and image to welcomeView

--- Bridges/WelcomeViewController.swift
+++ Bridges/WelcomeViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
     
+        welcomeViewLabel.text = "Welcome"
+        welcomeTextLabel.text = "Bridges is an app to find interesting bridges in you direct neighbourhood"
+        explanationTextLabel.text = "Explanation: try this"
+        welcomeImage.image = UIImage(named: "erasmusWelcome.png")
+        
+        
+        
+    
+    }
+    
+    @IBOutlet var welcomeViewLabel: UILabel!
+
+    @IBOutlet var welcomeTextLabel: UILabel!
+    
+    @IBOutlet var explanationTextLabel: UILabel!
+
+    @IBOutlet var welcomeImage: UIImageView!
     
 }
