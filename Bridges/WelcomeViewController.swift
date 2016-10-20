b6efdd85eae72d0751f128b2ddfe68dc47e57ba7
Thijs Lucassen <thijs@nessacul.nl>
2016-10-20 12:56:17 +0200

    Added ViewControllers

---
+++ Bridges/WelcomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WelcomeViewController.swift
+//  Bridges
+//
+//  Created by Thijs Lucassen on 20-10-16.
+//  Copyright © 2016 TL. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    
+    
+}
