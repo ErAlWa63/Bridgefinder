1bd2222ba68da3989d7900ef2ee9fa6afcca3c6e
Erik Waterham <waterham@gmail.com>
2016-10-31 17:39:40 +0100

    - BridgeObject van Struct naar Class om MapAnnotations te ondersteunen bij inlezen database; - Onze description heet nu descript; - DataSource is opgeschoond in commentaar; - MapViewController gebruikt nu viewDidAppear vanwege kenmerken van tab bar controller, tevens verbeteringen aan LocationManager toegevoegd;

--- Bridges/BridgeObject.swift
+++ Bridges/BridgeObject.swift
@@ -5,45 +5,57 @@
 //  Created by Erik Waterham on 26/10/2016.
 //  Copyright © 2016 TL. All rights reserved.
 //
+//  Base on: http://sweettutos.com/2016/01/21/swift-mapkit-tutorial-series-how-to-customize-the-map-annotations-callout-request-a-transit-eta-and-launch-the-transit-directions-to-your-destination/
 
 import Foundation
 import Firebase
+import MapKit
 
-struct BridgeObject {
+class BridgeObject: NSObject, MKAnnotation {
     
     let name: String
-    let description: String
+    let descript: String
     let image: String
     let latitude: Double
     let longitude: Double
     let ref: FIRDatabaseReference?
     let key: String
+
+    let coordinate: CLLocationCoordinate2D
+    let title: String?
+    let distance: Double?
     
     init(name: String, description: String, image: String, latitude: Double, longitude: Double, key: String = "") {
         self.key = key
         self.name = name
-        self.description = description
+        self.descript = description
         self.image = image
         self.latitude = longitude
         self.longitude = latitude
         self.ref = nil
+        self.title = name
+        self.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        self.distance = 0
     }
     
     init(snapshot: FIRDataSnapshot) {
         let snapshotValue = snapshot.value as! [String: AnyObject]
         name = snapshot.key
-        description = snapshotValue["Description"] as! String
+        descript = snapshotValue["Description"] as! String
         image = snapshotValue["Image"] as! String
         latitude = snapshotValue["Latitude"] as! Double
         longitude = snapshotValue["Longitude"] as! Double
         key = snapshot.key
         ref = snapshot.ref
+        title = name
+        coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        self.distance = 0
     }
     
     func toAnyObject() -> Any {
         return [
             "Name": name,
-            "Description": description,
+            "Description": descript,
             "Image": image,
             "Latitude": latitude,
             "Longitude": longitude
